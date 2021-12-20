3b122a4f16ef5ed9385843ea8e387bcb9e0bc5a5
sunitsen <sunitsen50@gmail.com>
2021-12-20 10:27:59 +0600

    message

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "300px";
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+
+//language selector................
+
